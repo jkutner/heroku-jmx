ff07c203e14db9ef10e65dcf7ba02794247dc428
Joe Kutner <jpkutner@gmail.com>
2015-10-14 19:51:21 -0500

    Set version to 1.0.1

--- commands/init.js
+++ commands/init.js
@@ -15,7 +15,7 @@ module.exports = {
     child.execSync('heroku buildpacks:add -i 1 https://codon-buildpacks.s3.amazonaws.com/buildpacks/jkutner/jmx.tgz -a ' + context.app)
 
     console.log('Setting config vars...')
-      child.execSync(`heroku config:set HEROKU_PROXY_URL=\"https:\/\/${process.env.HEROKU_PROXY_AUTH}@${process.env.HEROKU_PROXY_HOST}\" -a ${context.app}`)
+    child.execSync(`heroku config:set HEROKU_PROXY_URL=\"https:\/\/${process.env.HEROKU_PROXY_AUTH}@${process.env.HEROKU_PROXY_HOST}\" -a ${context.app}`)
 
     console.log('Done.')
   })
