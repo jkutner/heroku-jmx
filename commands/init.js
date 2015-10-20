7671d791b8f4556ef31584e2959a438ab39fd0c7
Joe Kutner <jpkutner@gmail.com>
2015-10-20 09:25:47 -0500

    Added dyno meta data to setup

--- commands/init.js
+++ commands/init.js
@@ -11,12 +11,15 @@ module.exports = {
   needsApp: true,
   needsAuth: true,
   run: cli.command(function (context, heroku) {
-    console.log('Setting JMX buildpack...')
-    child.execSync('heroku buildpacks:add -i 1 https://codon-buildpacks.s3.amazonaws.com/buildpacks/jkutner/jmx.tgz -a ' + context.app)
+    heroku.apps(context.app).info(function (err, app) {
+      console.log('Setting JMX buildpack...')
+      child.execSync('heroku buildpacks:add -i 1 https://codon-buildpacks.s3.amazonaws.com/buildpacks/jkutner/jmx.tgz -a ' + context.app)
 
-    console.log('Setting config vars...')
-    child.execSync(`heroku config:set HEROKU_PROXY_URL=\"https:\/\/${process.env.HEROKU_PROXY_AUTH}@${process.env.HEROKU_PROXY_HOST}\" -a ${context.app}`)
+      console.log('Setting config vars...')
+      child.execSync(`heroku config:set HEROKU_PROXY_URL=\"https:\/\/${process.env.HEROKU_PROXY_AUTH}@${process.env.HEROKU_PROXY_HOST}\" -a ${context.app}`)
+        child.execSync(`heroku config:set HEROKU_APP_ID=\"${app.id}\" -a ${context.app}`)
 
-    console.log('Done.')
+      console.log('Done.')
+    })
   })
 }
